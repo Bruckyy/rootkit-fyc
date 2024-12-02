10f6af850c2d70f42334e4e360d2ec5f9e8cd0e5
Bruckyy <bouncebloc95@gmail.com>
2024-12-03 00:31:10 +0100

    Exercice2_Partie1

---
+++ exercices/exercice-2/src/process/shadowed_process.rs
@@ -0,0 +1,33 @@
+//////////////////////////
+// PARTIE 1 (EXERCICE 2)//
+//////////////////////////
+
+use wdk_sys::LIST_ENTRY;
+
+// Identifier les offset des champs qui nous intéressent (à récupérer sur windbg ou vergilius project)
+const ACTIVE_PROCESS_LINKS_OFFSET: usize;
+const PID_OFFSET: usize;
+
+
+#[derive(Clone)]
+pub struct ShadowedProcess {
+    // La structure représentant le procesus à faire disparaître, doit contenir à minima trois attributs :
+
+    // Un pointeur sur la structure EPROCESS du processus
+    // Un pointeur sur le PID du processus
+    // Un pointeur sur la active_process_links (LIST_ENTRY)
+
+    // Vous êtes libre d'en ajouter d'autres si vous trouvez cela pertinent.
+}
+
+impl ShadowedProcess {
+    pub unsafe fn from_eprocess(eprocess: PVOID) -> Self {
+        // Permet de créer une structure ShadowedProcess à partir d'un pointeur sur EPROCESS
+        // Affecter les champs pid, active_process_links (LIST_ENTRY) à l'aide des offset définit plus tôt
+    }
+
+    pub unsafe fn next(&self) -> Self {
+        // Renvoie la structure ShadowedProcess du processus suivant dans la liste chaînée.
+    }
+
+}
